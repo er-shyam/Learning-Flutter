c6c3460389deebd081c70eb6354743ff1eb1c792
Shyam Thakur <er.shyam.work@gmail.com>
2025-05-01 01:16:25 +0530

    removed the use of print and instead of that used devtool.log

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -62,14 +63,14 @@ class _RegisterViewState extends State<RegisterView> {
                       email: email,
                       password: password,
                     );
-                print(userCredential);
+                devtools.log(userCredential.toString());
               } on FirebaseAuthException catch (e) {
                 if (e.code == '') {
-                  print('weak password');
+                  devtools.log('weak password');
                 } else if (e.code == 'email-already-in-use') {
-                  print("Email is already in use by shyam");
+                  devtools.log("Email is already in use by shyam");
                 } else if (e.code == 'invalid-email') {
-                  print('Email is invalid');
+                  devtools.log('Email is invalid');
                 }
               }
             },
